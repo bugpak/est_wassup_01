0f1fe8c6cb2ca8f542f84df4a7bdd1df645db401
yu seong lee <leeus96@naver.com>
2023-12-05 00:18:06 +0000

    LRscheduler, graph(loss,LR) and callback  add

--- ANN_pipeline/nn/early_stop.py
+++ ANN_pipeline/nn/early_stop.py
@@ -1,3 +1,5 @@
+import torch
+
 class EarlyStopper:
     '''
     function for Early Stopping of learning epochs 
@@ -15,8 +17,10 @@ class EarlyStopper:
         self.counter = 0
         self.min_validation_loss = float('inf')
 
-    def early_stop(self, validation_loss:float):
+    def early_stop(self, model, validation_loss:float, name='test.pth', mode=True):
         if validation_loss < self.min_validation_loss:
+            if mode:
+                torch.save(model.state_dict(), name)
             self.min_validation_loss = validation_loss
             self.counter = 0
         elif validation_loss > (self.min_validation_loss + self.min_delta):
